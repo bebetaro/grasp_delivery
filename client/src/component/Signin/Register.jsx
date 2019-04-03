2c7b80d597bb56fbeeab41546d105fcdd66c8590
JIM10-151005 <t.kurokawa@kobayashi-mfg.co.jp>
2019-04-04 00:54:14 +0900

    Modify location of buttons

--- client/src/component/Signin/Register.jsx
+++ client/src/component/Signin/Register.jsx
@@ -1,29 +1,29 @@
 //This component should be linked to Login component after registered to give user login status and redirect
 //After call to server side, request will be switched to "Already registered" or "New registerd"
 
-import _ from "lodash";
-import React, { Component } from "react";
-import { reduxForm, Field } from "redux-form";
-import { Link, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import inputField from "./inputField";
-import validate from "../../utils/validationUser";
-import asyncValidate from "../../utils/asyncValidateUser";
-import registerField from "./registerField";
-import * as actions from "../../actions";
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import inputField from './inputField';
+import validate from '../../utils/validationUser';
+import asyncValidate from '../../utils/asyncValidateUser';
+import registerField from './registerField';
+import * as actions from '../../actions';
 
 class Register extends Component {
   componentWillUpdate() {
     // if login is success, redirect to mypage
     //console.log(this.props.auth);
     if (this.props.auth) {
-      this.props.history.push("/mypage"); //here push
+      this.props.history.push('/mypage'); //here push
     }
   }
 
   renderFields() {
     return _.map(registerField, ({ label, name, helpertext }) => {
-      const selector = name === "password" ? "password" : "text";
+      const selector = name === 'password' ? 'password' : 'text';
       return (
         <Field
           key={name}
@@ -42,20 +42,21 @@ class Register extends Component {
     return (
       <div className="row">
         <form
-          className="col s6 push-s3"
-          style={{ marginTop: "70px" }}
+          style={{ marginTop: '70px' }}
           onSubmit={handleSubmit(() =>
             submitRegister(formValues.values, history)
           )}
         >
           {this.renderFields()}
-          <Link to="/" className="red btn-flat white-text">
-            Cancel
-          </Link>
-          <button type="submit" className="teal btn-flat right white-text">
-            Register
-            <i className="material-icons right">send</i>
-          </button>
+          <div className="col s10 center-align">
+            <Link to="/" className="red btn-flat white-text">
+              Cancel
+            </Link>
+            <button type="submit" className="teal btn-flat right white-text">
+              Register
+              <i className="material-icons right">send</i>
+            </button>
+          </div>
         </form>
       </div>
     );
@@ -70,11 +71,14 @@ const mapStatetoProps = state => {
 };
 
 export default reduxForm({
-  form: "signupForm",
+  form: 'signupForm',
   validate,
   asyncValidate,
-  asyncBlurFields: ["username"]
+  asyncBlurFields: ['username']
 })(
   //Using "react-redux" "react-router-dom" "redux-form" at the same time
-  connect(mapStatetoProps, actions)(withRouter(Register))
+  connect(
+    mapStatetoProps,
+    actions
+  )(withRouter(Register))
 );
